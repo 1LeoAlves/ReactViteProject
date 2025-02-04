f834b74caf4f581e77800dc86787503084baa69d
Leo_Alves <89089486+1LeoAlves@users.noreply.github.com>
2025-02-03 21:56:18 -0300

    skilCard

--- src/assets/components/SkillCard.jsx
+++ src/assets/components/SkillCard.jsx
@@ -1,9 +1,8 @@
 export default function SkillCard(props){
     return(
-        <li>
-            <img src={props.src} alt="skil_Icon" />
-            <h3>Skil Title</h3>
-            <p>Tighe Descript</p>
+        <li className="flex flex-col bg-[#2d2d2d] p-3 rounded-[10px] " >
+            <img className="p-6 w-full" src={props.src} alt="skil_Icon" />
+            <h3 className="text-center">{props.title}</h3>
         </li>
     );
 }
--- src/assets/components/SkillsSection.jsx
+++ src/assets/components/SkillsSection.jsx
@@ -1,11 +1,20 @@
+import SkillCard from "../components/SkillCard"
+
 export default function SkillsSection(){
     return(
         <section className="w-full p-8 font-mono text-white">
             <div className="lg:flex drop-shadow-[3px_8px_7px_black] transition-drop-shadow duration-500 hover:drop-shadow-[0px_0px_7px_#FFFFFFE3] xl:gap-8 bg-[#0B0B0B] p-7">
                 <div className="w-full">
                     <h1 className="flex justify-center align-middle bg-gradient-to-r from-[rgb(64,7,64)] via-[rgb(35,11,58)] to-[rgb(68,9,82)] font-bold min-md:text-3xl  xl:text-5xl p-3 rounded-[.25rem]">Linguagens e Ferramentas</h1>
-                    <ul className="grid-cols-4">
-                        <SkillCard></SkillCard>
+                    <ul className="pt-5 gap-5 grid grid-rows-4 grid-cols-4">
+                        <SkillCard title={"HTML/CSS"} src={"./vite.svg"}></SkillCard>
+                        <SkillCard title={""} src={"./vite.svg"}></SkillCard>
+                        <SkillCard title={""} src={"./vite.svg"}></SkillCard>
+                        <SkillCard title={""} src={"./vite.svg"}></SkillCard>
+                        <SkillCard title={""} src={"./vite.svg"}></SkillCard>
+                        <SkillCard title={""} src={"./vite.svg"}></SkillCard>
+                        <SkillCard title={""} src={"./vite.svg"}></SkillCard>
+                        <SkillCard title={""} src={"./vite.svg"}></SkillCard>
                     </ul>
                 </div>
             </div>
