64734ae2e82017b381bf1eaf047001b7f85aa4f2
Leo_Alves <89089486+1LeoAlves@users.noreply.github.com>
2025-02-04 07:58:17 -0300

    Footer modify

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,8 +1,9 @@
 function Footer(){
     return (
         <section className="font-mono p-10 w-full h-80 bg-gradient-to-r from-[rgb(64,7,64)] via-[rgb(35,11,58)] to-[rgb(68,9,82)] flex">
-            <div>
-                <h1 className="text-white text-4xl font-bold">Contato</h1>
+            <h1 className="text-white text-4xl font-bold">Contato</h1>
+            <div >
+
             </div>
         </section>
     )
